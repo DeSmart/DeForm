e98067cedbb627c79de58f06f8cffc5f245b79f7
fen <fojuth@gmail.com>
2014-11-13 23:57:15 +0100

    Handling form element's getters and setters.

--- spec/DeFormSpec.php
+++ spec/DeFormSpec.php
@@ -5,9 +5,28 @@ namespace spec\DeForm;
 use PhpSpec\ObjectBehavior;
 use DeForm\Request\RequestInterface;
 use DeForm\Node\NodeInterface;
+use DeForm\Element\ElementInterface;
+use Prophecy\Prophet;
 
 class DeFormSpec extends ObjectBehavior
 {
+    
+    /**
+     * @var Prophet
+     */
+    private $prophet;
+    
+    /**
+     * @return Prophet
+     */
+    private function getProphet()
+    {
+        if (true === empty($this->prophet)) {
+            $this->prophet = new Prophet;
+        }
+        
+        return $this->prophet;
+    }
 
     function let(NodeInterface $formNode, RequestInterface $request)
     {
@@ -33,6 +52,45 @@ class DeFormSpec extends ObjectBehavior
         $request->get(\DeForm\DeForm::DEFORM_ID)->willReturn('bar');
         
         $this->isSubmitted()->shouldReturn(false);
+    }
+    
+    function it_should_throw_exception_while_setting_same_element_twice(ElementInterface $element)
+    {
+        $element->getName()->willReturn('foo');
+        
+        $this->setElement($element);
+        
+        $this->shouldThrow('\LogicException')->during('setElement', array($element));
+    }
+    
+    function it_should_throw_exception_while_getting_nonexisting_element()
+    {
+        $this->shouldThrow('\LogicException')->during('getElement', array('bar'));
+    }
+    
+    function it_should_set_element_values_with_values_from_request()
+    {
+        $requestData = array(
+            'field_1' => 'foo',
+            'field_2' => 42,
+        );
+        
+        // Stub the form elements and register them in the form instance
+        foreach ($requestData as $fieldName => $value) {
+            $element = $this->getProphet()->prophesize('\DeForm\Element\ElementInterface');
+            
+            $element->getName()->willReturn($fieldName);
+            $element->setValue($value)->shouldBeCalled();
+            $element->getValue()->willReturn($value);
+            
+            $this->setElement($element);
+        }
+        
+        $this->setElementsValues($requestData);
+        
+        foreach ($requestData as $fieldName => $value) {
+            $this->getElement($fieldName)->getValue()->shouldReturn($value);
+        }
     }
 
 }
--- src/DeForm.php
+++ src/DeForm.php
@@ -16,6 +16,11 @@ class DeForm
      */
     protected $formNode;
     
+    /**
+     * @var \DeForm\Element\ElementInterface[]
+     */
+    protected $elements = array();
+    
     const DEFORM_ID = '__deform_id';
 
     public function __construct(NodeInterface $formNode, RequestInterface $request)
@@ -38,4 +43,42 @@ class DeForm
         return false;
     }
 
+    public function setElementsValues($data)
+    {
+        foreach ($data as $fieldName => $value) {
+            $this->getElement($fieldName)->setValue($value);
+        }
+    }
+
+    /**
+     * @todo Write desc.
+     * 
+     * @param \DeForm\Element\ElementInterface $element
+     */
+    public function setElement($element)
+    {
+        $name = $element->getName();
+        
+        if (true === key_exists($name, $this->elements)) {
+            throw new \LogicException(sprintf('Cannot set the element "%s" more than once (same name)', $name));
+        }
+        
+        $this->elements[$element->getName()] = $element;
+    }
+
+    /**
+     * @todo Write desc.
+     * 
+     * @param string $name
+     * @return \DeForm\Element\ElementInterface
+     * @throws \LogicException
+     */
+    public function getElement($name)
+    {
+        if (false === key_exists($name, $this->elements)) {
+            throw new \LogicException(sprintf('Trying to get a non-existing element ("%s")', $name));
+        }
+        
+        return $this->elements[$name];
+    }
 }
