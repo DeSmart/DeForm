b88adf4cc8da5a412a6efdb91fd086032eca6924
fen <fen@desmart.com>
2014-11-28 11:05:02 +0100

    Minor refactoring.

--- src/DeForm.php
+++ src/DeForm.php
@@ -2,25 +2,26 @@
 
 use DeForm\Node\NodeInterface;
 use DeForm\Request\RequestInterface;
+use DeForm\Element\ElementInterface;
 
 class DeForm
 {
-    
+
     /**
-     * @var \DeForm\Request\RequestInterface
+     * @var RequestInterface
      */
     protected $request;
-    
+
     /**
-     * @var \DeForm\Node\NodeInterface
+     * @var NodeInterface
      */
     protected $formNode;
-    
+
     /**
-     * @var \DeForm\Element\ElementInterface[]
+     * @var ElementInterface[]
      */
     protected $elements = [];
-    
+
     const DEFORM_ID = '__deform_id';
 
     public function __construct(NodeInterface $formNode, RequestInterface $request)
@@ -31,13 +32,13 @@ class DeForm
 
     /**
      * Returns true if the form was submitted, false otherwise.
-     * 
+     *
      * @return boolean
      */
     public function isSubmitted()
     {
         if ($this->formNode->getAttribute('name') === $this->request->get(static::DEFORM_ID)) {
-          return true;
+            return true;
         }
 
         return false;
@@ -45,34 +46,34 @@ class DeForm
 
     /**
      * Registers a new form element.
-     * 
-     * @param \DeForm\Element\ElementInterface $element
+     *
+     * @param ElementInterface $element
      * @return self
      */
-    public function addElement($element)
+    public function addElement(ElementInterface $element)
     {
         $name = $element->getName();
-        
+
         if (true === array_key_exists($name, $this->elements)) {
             throw new \LogicException(sprintf('Cannot set the element "%s" more than once (same name)', $name));
         }
-        
+
         $request_value = $this->request->get($name);
-        
-        if (true === $this->isSubmitted() && null !== $request_value && false === $element->isReadonly()) {
+
+        $this->elements[$name] = $element;
+
+        if (true === $this->shouldSetValue($element, $request_value)) {
             $element->setValue($request_value);
         }
-        
-        $this->elements[$name] = $element;
-        
+
         return $this;
     }
 
     /**
      * Returns a form element by its name.
-     * 
+     *
      * @param string $name
-     * @return \DeForm\Element\ElementInterface
+     * @return ElementInterface
      * @throws \LogicException
      */
     public function getElement($name)
@@ -80,7 +81,31 @@ class DeForm
         if (false === array_key_exists($name, $this->elements)) {
             throw new \LogicException(sprintf('Trying to get a non-existing element ("%s")', $name));
         }
-        
+
         return $this->elements[$name];
+    }
+
+    /**
+     * Returns true if the element's value should be set.
+     *
+     * @param ElementInterface $element
+     * @param string $value
+     * @return bool
+     */
+    protected function shouldSetValue(ElementInterface $element, $value)
+    {
+        if (false === $this->isSubmitted()) {
+            return false;
+        }
+
+        if (null === $value) {
+            return false;
+        }
+
+        if (true === $element->isReadonly()) {
+            return false;
+        }
+
+        return true;
     }
 }
